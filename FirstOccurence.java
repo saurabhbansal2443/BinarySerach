244c9ee9a157afdd5dcffc0e8a1d77881deafb06
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-06-23 16:43:44 +0530

    last occurence

---
+++ FirstOccurence.java
@@ -0,0 +1,34 @@
+public class FirstOccurence {
+    public static void main(String[] args) {
+         int[] arr = {10,20,20,20,20,20,20,30,40,50};
+
+        int k = 20;
+
+        int ans = firstOccurence(arr, k);
+
+        System.out.println(ans);
+    }
+
+    public static int firstOccurence(int[] arr , int k){
+
+
+        int start=0;
+        int end = arr.length-1;
+        int ans =-1;
+        while(start<=end){
+
+            int mid = (start+end)/2;
+
+            if(arr[mid]==k){
+                ans = mid ;
+                start= mid+1;
+            }else if ( k<arr[mid]){
+                end = mid-1;
+            }else if ( k>arr[mid]){
+                start=mid+1;
+            }
+        }
+
+        return ans ;
+    }
+}
---
+++ lastOccurence.java
@@ -0,0 +1,34 @@
+public class lastOccurence {
+    public static void main(String[] args) {
+        int[] arr = {10,20,20,20,20,20,20,30,40,50};
+
+        int k = 20;
+
+        int ans = lastOccurence(arr, k);
+
+        System.out.println(ans);
+    }
+
+    public static int lastOccurence(int[] arr , int k){
+
+
+        int start=0;
+        int end = arr.length-1;
+        int ans =-1;
+        while(start<=end){
+
+            int mid = (start+end)/2;
+
+            if(arr[mid]==k){
+                ans = mid ;
+                start= mid+1;
+            }else if ( k<arr[mid]){
+                end = mid-1;
+            }else if ( k>arr[mid]){
+                start=mid+1;
+            }
+        }
+
+        return ans ;
+    }
+}
