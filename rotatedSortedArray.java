cd15a625ba5357a0c3e4eb62f2eafa0aa4f5a5b4
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-06-23 17:46:57 +0530

    rotated sorted array

--- rotatedSortedArray.java
+++ rotatedSortedArray.java
@@ -1,8 +1,8 @@
 public class rotatedSortedArray {
     public static void main(String[] args) {
-        int[] arr = { 100, 110, 120, 130, 10, 20, 30, 40, 50 };
+        int[] arr = { 70,80,90,10,20,30,40,50,60};
 
-        int k = 130;
+        int k = 10;
 
         int ans = search(arr, k);
 
@@ -20,13 +20,13 @@ public class rotatedSortedArray {
 
             if (arr[mid] == k) {
                 return mid;
-            } else if (arr[start] < arr[mid]) { // left part sorted
+            } else if (arr[start] <= arr[mid]) { // left part sorted
                 if (k >= arr[start] && k < arr[mid]) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
-            } else if (arr[end] > arr[mid]) { // right part sorted
+            } else if (arr[end] >= arr[mid]) { // right part sorted
 
                 if (k > arr[mid] && k <= arr[end]) {
                     start = mid + 1;
